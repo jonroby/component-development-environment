81d1b35733ed47a63a0cb7e1ed1df8bf7db63f36
jonroby <joncaroby@gmail.com>
2018-05-24 19:31:45 -0700

    component bar

---
+++ src/CDE/ComponentBar.js
@@ -0,0 +1,37 @@
+// @flow
+
+import React, { Component } from "react";
+
+import "./ComponentBar.css";
+
+const ComponentBar = props => {
+  const { components, selected, onSelect } = props;
+  const displayComponents = (components, selected, onSelect) => {
+    return components.map(comp => {
+      console.log('comp ', comp)
+      const selectedStyle = selected === comp ? "component-bar-links-selected" : "";
+      console.log('selected ', selected)
+
+      return (
+
+        <li className={selectedStyle} onClick={e => onSelect(e, comp)}>
+          {comp}
+        </li>
+      );
+    });
+  };
+
+  return (
+    <div className="component-bar-container">
+      <div className="component-bar-header">
+        <div>Component Development Environment</div>
+      </div>
+
+      <ul className="component-bar-links-list">
+        {displayComponents(components, selected, onSelect)}
+      </ul>
+    </div>
+  );
+};
+
+export default ComponentBar;
