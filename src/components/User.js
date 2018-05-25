f837aacaae0c92cfcf550897dcc4d3ef80673524
jonroby <joncaroby@gmail.com>
2018-05-24 18:25:54 -0700

    add src components dir, test components and component props file

---
+++ src/components/User.js
@@ -0,0 +1,35 @@
+// @flow
+
+import React, { Component } from 'react';
+
+import './User.css';
+
+// type test = {
+//   isSubscriber: boolean,
+// }
+
+type Props = {
+  userTitle: string,
+  userId: number,
+  userFavoriteNumbers: Array<number>
+};
+
+class User extends Component<Props> {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const userIds = this.props.userId ? this.props.userId : [];
+    return (
+        <div className="User">
+        <div>User</div>
+        <div>user name: {this.props.userName}</div>
+        <div>user id: {this.props.userId}</div>
+        <div>user favorite numbers: {userFavoriteNumbers.map(i => <div>{i}</div>)}</div>
+        </div>
+    );
+  }
+}
+
+export default User;
