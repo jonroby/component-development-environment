1a5a07ca975ad53c1e6c6025aa4b57913e2972d1
jonroby <joncaroby@gmail.com>
2018-05-26 19:33:57 -0700

    add component bar cdescreen

--- src/CDE/CDE.js
+++ src/CDE/CDE.js
@@ -4,34 +4,87 @@ import React, { Component } from "react";
 
 import "./CDE.css";
 
-// import TypesDisplay from "./TypesDisplay";
+import TypesTable from "./TypesTable";
 
 class CDE extends Component<Props> {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      selectedTab: "component"
+    };
   }
 
-  render() {
-    console.log("this.props.componentProps ", this.props.componentProps);
-    let displayName = this.props.componentProps.displayName || "";
+  renderSelectedTab = element => {
+    const types = (
+      <TypesTable componentProps={this.props.componentProps.props} />
+    );
 
-    // const el = this.props.selected ? components[this.props.selected] : false;
-    // const element = el ? React.createElement(el, fakeProps, null) : null;
+    const component = (
+      <div className="cde-component-container">
+        <div className="cde-component-outer">
+          <div className="cde-component">{element}</div>
+        </div>
+      </div>
+    );
+
+    const code = (
+      <pre className="code">{this.props.componentProps.filecontents}</pre>
+    );
+    const opts = { types, component, code };
+    return opts[this.state.selectedTab];
+  };
+
+  render() {
+    const el = this.props.selectedComponent
+      ? this.props.components[this.props.selectedComponent]
+      : false;
+    const element = el ? React.createElement(el, null, null) : null;
 
     return (
       <div className="cde">
         <div>
           <div className="cde-component-name">
-            <div>{displayName}</div>
+            <div>{this.props.selectedComponent}</div>
           </div>
+
+          <div className="cde-tab-bar">
+            <div
+              className={
+                this.state.selectedTab === "component"
+                  ? "cde-selected-tab"
+                  : "cde-tab"
+              }
+              onClick={e => this.setState({ selectedTab: "component" })}
+            >
+              Component
+            </div>
+            <div
+              className={
+                this.state.selectedTab === "types"
+                  ? "cde-selected-tab"
+                  : "cde-tab"
+              }
+              onClick={e => this.setState({ selectedTab: "types" })}
+            >
+              Types
+            </div>
+            <div
+              className={
+                this.state.selectedTab === "code"
+                  ? "cde-selected-tab"
+                  : "cde-tab"
+              }
+              onClick={e => this.setState({ selectedTab: "code" })}
+            >
+              Code
+            </div>
+          </div>
+
+          {this.renderSelectedTab(element)}
         </div>
       </div>
     );
   }
 }
-
-// <TypesDisplay />
-//  <div className="cde-component">{element}</div>
 
 export default CDE;
