8f6af1a3c8420c6c6c925656d20e0f6955674c08
jonroby <joncaroby@gmail.com>
2018-05-31 14:07:13 -0700

    component view

---
+++ src/CDE/ComponentView.js
@@ -0,0 +1,30 @@
+import React from "react";
+import * as components from "../components/index";
+
+import "./ComponentView.css";
+
+function ComponentView(props) {
+  const selectedComponent =
+    props.selectedComponent && components[props.selectedComponent];
+
+  const element =
+    selectedComponent && props.fakeProps
+      ? React.createElement(
+          selectedComponent,
+          props.fakeProps[props.selectedComponent],
+          null
+        )
+      : null;
+
+  console.log("here");
+
+  return (
+    <div className="cde-component-container">
+      <div className="cde-component-outer">
+        <div className="cde-component">{element}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ComponentView;
