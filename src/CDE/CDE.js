638f65e98478660dcbf2eab5019ebf1da79da967
jonroby <joncaroby@gmail.com>
2018-05-31 11:19:51 -0700

    map state to props selected component

--- src/CDE/CDE.js
+++ src/CDE/CDE.js
@@ -1,22 +1,58 @@
 // @flow
 
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 import "./CDE.css";
 
 import TypesTable from "./TypesTable";
+import SelectFakeProps from "./SelectFakeProps.js";
 
 class CDE extends Component<Props> {
   constructor(props) {
     super(props);
     this.state = {
-      selectedTab: "component"
+      selectedTab: "component",
+      fakeProps: {},
+      snapshots: ["default"],
+      selectedSnapshot: "default"
     };
+  }
+
+  componentDidMount() {
+    fetch(`http://localhost:3000/fake_props/${this.props.selectedComponent}`)
+      .then(response => {
+        console.log("response ", response);
+        return response.json();
+      })
+      .then(fakeProps => {
+        if (!fakeProps) return;
+        this.setState({ fakeProps });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.selectedComponent === this.props.selectedComponent) return;
+
+    fetch(`http://localhost:3000/fake_props/${this.props.selectedComponent}`)
+      .then(response => {
+        return response.json();
+      })
+      .then(fakeProps => {
+        if (!fakeProps) return;
+        this.setState({ fakeProps });
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
   renderSelectedTab = element => {
     const types = (
-      <TypesTable componentProps={this.props.componentProps.props} />
+      <TypesTable selectedComponent={this.props.selectedComponent} />
     );
 
     const component = (
@@ -27,24 +63,47 @@ class CDE extends Component<Props> {
       </div>
     );
 
-    const code = (
-      <pre className="code">{this.props.componentProps.filecontents}</pre>
+    const custom = (
+      <SelectFakeProps
+        selectedComponent={this.props.selectedComponent}
+        updateSnapshots={this.updateSnapshots}
+        selectedSnapshot={this.selectedSnapshot}
+      />
     );
-    const opts = { types, component, code };
+
+    // const code = (
+    //   <pre className="code">{this.props.componentProps.filecontents}</pre>
+    // );
+
+    const code = null;
+
+    const opts = { types, component, code, custom };
     return opts[this.state.selectedTab];
+  };
+
+  updateSnapshots = snapshots => {
+    this.setState({ snapshots });
   };
 
   render() {
     const el = this.props.selectedComponent
       ? this.props.components[this.props.selectedComponent]
       : false;
-    const element = el ? React.createElement(el, null, null) : null;
+
+    // console.log('this.props.componentProps ', this.props.componentProps)
+    // const selectedComponentProps = this.props.componentProps
+    console.log("this.state.fakeProps ", this.state.fakeProps);
+
+    const element = el
+      ? React.createElement(el, this.state.fakeProps, null)
+      : null;
 
     return (
       <div className="cde">
         <div>
           <div className="cde-component-name">
             <div>{this.props.selectedComponent}</div>
+            <div>{this.state.snapshots.map(i => <div>{i}</div>)}</div>
           </div>
 
           <div className="cde-tab-bar">
@@ -60,6 +119,17 @@ class CDE extends Component<Props> {
             </div>
             <div
               className={
+                this.state.selectedTab === "custom"
+                  ? "cde-selected-tab"
+                  : "cde-tab"
+              }
+              onClick={e => this.setState({ selectedTab: "custom" })}
+            >
+              Custom
+            </div>
+
+            <div
+              className={
                 this.state.selectedTab === "types"
                   ? "cde-selected-tab"
                   : "cde-tab"
@@ -68,6 +138,7 @@ class CDE extends Component<Props> {
             >
               Types
             </div>
+
             <div
               className={
                 this.state.selectedTab === "code"
@@ -87,4 +158,9 @@ class CDE extends Component<Props> {
   }
 }
 
-export default CDE;
+const mapStateToProps = state => ({
+  components: state.cde.components,
+  selectedComponent: state.cde.selectedComponent
+});
+
+export default connect(mapStateToProps)(CDE);
