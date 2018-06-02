0333894c4e103031e5039daaaf7a277b9ead63cb
jonroby <joncaroby@gmail.com>
2018-06-01 22:06:30 -0700

    make CDEScreen a main source of data

--- src/CDE/CDEScreen.js
+++ src/CDE/CDEScreen.js
@@ -1,13 +1,34 @@
 // @flow
 
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
+import { fetchComponentData } from "../redux/actions/cde";
 import CDE from "./CDE";
 import ComponentBar from "./ComponentBar";
 
 import "./CDEScreen.css";
 
 class CDEScreen extends Component {
+  componentDidMount() {
+    this.props.fetchComponentData({
+      component: this.props.selectedComponent,
+      snapshot: this.props.selectedSnapshot
+    });
+  }
+
+  componentDidUpdate(prevProps) {
+    if (
+      this.props.selectedSnapshot !== prevProps.selectedSnapshot ||
+      this.props.selectedComponent !== prevProps.selectedComponent
+    ) {
+      this.props.fetchComponentData({
+        component: this.props.selectedComponent,
+        snapshot: this.props.selectedSnapshot
+      });
+    }
+  }
+
   render() {
     return (
       <div className="cde-screen">
@@ -19,4 +40,9 @@ class CDEScreen extends Component {
   }
 }
 
-export default CDEScreen;
+const mapStateToProps = state => ({
+  selectedComponent: state.cde.selectedComponent,
+  selectedSnapshot: state.cde.selectedSnapshot
+});
+
+export default connect(mapStateToProps, { fetchComponentData })(CDEScreen);
