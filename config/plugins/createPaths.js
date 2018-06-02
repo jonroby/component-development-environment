ec7ed33d7dc0ee967763544e4503896c0cc0ac47
jonroby <joncaroby@gmail.com>
2018-06-02 07:11:25 -0700

    createPaths helper for CdePlugin

---
+++ config/plugins/createPaths.js
@@ -0,0 +1,122 @@
+const fs = require("fs");
+const faker = require("faker");
+const changeCase = require("change-case");
+const reactDocs = require("react-docgen");
+
+function isObject (value) {
+  return value && typeof value === 'object' && value.constructor === Object;
+}
+
+function createFakeProps(flowData, opts) {
+  const fakeComponentProps = Object.keys(flowData.props).reduce(
+    (acc, propKey) => {
+      const prop = flowData.props[propKey];
+      const path = propKey;
+      const paths = {};
+      const fakeProp = createFakeProp(prop.flowType, paths, path);
+      return Object.assign({}, acc, fakeProp);
+    },
+    {}
+  );
+
+  return fakeComponentProps;
+}
+
+function createFakeProp(prop, paths, path) {
+  switch (changeCase.lowerCase(prop.name)) {
+    case "number":
+      return createNumber(prop, paths, path + '/number');
+
+    case "string":
+      return createString(prop, paths, path + '/string');
+
+    case "boolean":
+      return createBoolean(prop, paths, path + '/boolean');
+
+    case "void":
+      return null; // undefined?
+
+    case "array":
+      return createArray(prop, paths, path + '/array');
+
+    case "signature":
+      if (prop.type === "object") {
+        return createObject(prop, paths, path + '/object');
+      } else if (prop.type === "function") {
+        return createFunction(prop, paths, path + '/function');
+      }
+
+    case "union":
+      return createUnion(prop, paths, path + '/union');
+
+    case "literal":
+      // TODO: react-docgen changes true to 'true', 1 to '1', 'hello' to '"hello"'
+      return createLiteral(prop, paths, path + '/literal');
+
+    default:
+      console.log("No match.");
+  }
+}
+
+function createNumber(prop, paths, path) {
+  return { [path]: 'default' };
+}
+
+function createString(prop, paths, path) {
+  return { [path]: 'default' };
+}
+
+function createBoolean(prop, paths, path) {
+  return { [path]: 'default' };
+}
+
+function createLiteral(prop, paths, path) {
+  if (prop.elements) {
+    return prop.elements.reduce(p => {
+      return createFakeProp(p, paths, path);
+    });
+  }
+
+  return { [path]: 'default' };
+}
+
+function createUnion(prop, paths, path) {
+  // return createFakeProp(prop.elements[0], paths, path);
+  const red = prop.elements.reduce((acc, curr) => {
+    let el = createFakeProp(curr, paths, path);
+    return Object.assign({}, acc, el);
+  }, {});
+
+  return { [path]: red };
+}
+
+function createArray(prop, paths, path) {
+  const red = prop.elements.reduce((acc, curr) => {
+    let el = createFakeProp(curr, paths, path);
+    return Object.assign({}, acc, el);
+  }, {});
+
+  return { [path]: red };
+}
+
+function createObject(prop, paths, path) {
+  const { properties } = prop.signature;
+  const red = properties.reduce((acc, curr) => {
+    const addPath = isObject(curr.key) ? `/object/${curr.key}` : `/${curr.key}`; 
+    const keyVal = {
+      [curr.key]: createFakeProp(curr.value, paths, path + addPath)
+    };
+
+    return Object.assign({}, acc, keyVal);
+  }, {});
+
+  return { [path]: red };
+}
+
+function createFunction(prop, paths, path) {
+  const { properties } = prop.signature;
+  const ret = createFakeProp(prop.signature.return);
+  return () => ret;
+}
+
+module.exports = createFakeProps;
