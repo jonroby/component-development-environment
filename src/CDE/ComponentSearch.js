abff5d2f4671834c7c549baae11db0880ec0b980
jonroby <joncaroby@gmail.com>
2018-06-07 23:40:34 -0700

    component search

---
+++ src/CDE/ComponentSearch.js
@@ -0,0 +1,34 @@
+// @flow
+
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Input } from "antd";
+import { AutoComplete } from "antd";
+import { updateComponentSearchInput } from "../redux/actions/cde";
+
+import "./ComponentSearch.css";
+
+class ComponentSearch extends Component {
+  render() {
+    return (
+      <div className="input-container">
+        <Input
+          placeholder="Search components"
+          value={this.props.componentSearchInput}
+          onChange={event =>
+            this.props.updateComponentSearchInput(event.target.value)
+          }
+        />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  componentSearchInput: state.cde.componentSearchInput,
+  componentNames: state.cde.componentNames
+});
+
+export default connect(mapStateToProps, {
+  updateComponentSearchInput
+})(ComponentSearch);
