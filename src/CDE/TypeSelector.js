ab794e13d76235d5ec89547ad52960f923061239
jonroby <joncaroby@gmail.com>
2018-06-08 23:23:53 -0700

    change to class

--- src/CDE/TypeSelector.js
+++ src/CDE/TypeSelector.js
@@ -4,6 +4,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import changeCase from "change-case";
 import { Collapse, Checkbox, Button } from "antd";
+import { updateSnapshot } from "../redux/actions/cde.js";
 import CustomOptions from "./CustomOptions";
 import "./TypeSelector.css";
 const Panel = Collapse.Panel;
@@ -12,49 +13,68 @@ function isObject(value) {
   return value && typeof value === "object" && value.constructor === Object;
 }
 
-function TypeSelector(props) {
-  function renderInspector(flowAst, snapshot) {
+// const onClick = (option, suboption) => {
+//   this.props.updateSnapshot({
+//     [this.props.path]: { section: option, item: suboption }
+//   });
+
+//   this.setState({
+//     currentOption: suboption,
+//     showOptions: false,
+//     selectedOption: "",
+//     selectedSuboption: ""
+//   });
+// };
+
+class TypeSelector extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  renderInspector = (flowAst, snapshot) => {
     return Object.keys(flowAst.props).map((key, i) => {
       const required = flowAst.props[key].required;
       return (
         <Panel header={key} key={`${key}${i}`}>
           <div className="prop-type-container">
-            <Checkbox onChange={onChange} disabled={required}>
+            <Checkbox onChange={this.onChange} disabled={required}>
               Maybe
             </Checkbox>
             <div className="prop-type">
-              {render(flowAst.props[key].flowType, key)}
+              {this.caseRender(flowAst.props[key].flowType, key)}
             </div>
           </div>
         </Panel>
       );
     });
-  }
+  };
 
-  function onChange(e) {
+  onChange = e => {
     console.log(`checked = ${e.target.checked}`);
-  }
+  };
 
-  function render(prop, path) {
+  caseRender(prop, path) {
     switch (changeCase.lowerCase(prop.name)) {
       case "number":
-        return createPrimitive(prop, path + "/number");
+        return this.createPrimitive(prop, path + "/number");
 
       case "string":
-        return createPrimitive(prop, path + "/string");
+        return this.createPrimitive(prop, path + "/string");
 
       case "boolean":
-        return createPrimitive(prop, path + "/boolean");
+        return this.createPrimitive(prop, path + "/boolean");
 
       // case "void":
       //   return null; // undefined?
 
       case "array":
-        return createArray(prop, path + "/array");
+        return this.createArray(prop, path + "/array");
 
       case "signature":
         if (prop.type === "object") {
-          return createObject(prop, path + "/object");
+          return this.createObject(prop, path + "/object");
         }
 
         return null;
@@ -75,7 +95,7 @@ function TypeSelector(props) {
     }
   }
 
-  function createPrimitive(prop, path) {
+  createPrimitive(prop, path) {
     return (
       <div className="primitive-container">
         <div className="key">
@@ -85,43 +105,43 @@ function TypeSelector(props) {
         </div>
 
         <CustomOptions
-          snapshot={props.snapshot}
+          snapshot={this.props.snapshot}
           path={path}
-          currentOption={props.snapshot[path]}
+          currentOption={this.props.snapshot[path]}
         />
       </div>
     );
   }
 
-  function createBoolean(prop) {
+  createBoolean = prop => {
     // return (
     //   <div>
     //     <div>{prop.name}</div>
     //   </div>
     // );
-  }
+  };
 
-  function createLiteral(prop, paths, path) {
+  createLiteral = (prop, paths, path) => {
     // if (prop.elements) {
     //   return prop.elements.reduce(p => {
     //     return render(p, paths, path);
     //   });
     // }
     // return { [path]: "default" };
-  }
+  };
 
-  function createUnion(prop, paths, path) {
+  createUnion = (prop, paths, path) => {
     // return render(prop.elements[0], paths, path);
     // const red = prop.elements.reduce((acc, curr) => {
     //   let el = render(curr, paths, path);
     //   return Object.assign({}, acc, el);
     // }, {});
     // return { [path]: red };
-  }
+  };
 
-  function createArray(prop, path) {
+  createArray = (prop, path) => {
     const typesInArray = prop.elements.map(type => {
-      return render(type, path);
+      return this.caseRender(type, path);
     });
 
     return (
@@ -135,9 +155,9 @@ function TypeSelector(props) {
         <div className="value">{typesInArray.map(i => i)}</div>
       </div>
     );
-  }
+  };
 
-  function createObject(prop, path) {
+  createObject = (prop, path) => {
     const { properties } = prop.signature;
 
     const propertiesList = properties.map(p => {
@@ -152,7 +172,7 @@ function TypeSelector(props) {
             </Button>
           </div>
 
-          <div>{render(p.value, path)}</div>
+          <div>{this.caseRender(p.value, path)}</div>
         </div>
       );
     });
@@ -167,19 +187,23 @@ function TypeSelector(props) {
         <div className="value">{propertiesList}</div>
       </div>
     );
-  }
+  };
 
-  function createFunction(prop, paths, path) {
+  createFunction = (prop, paths, path) => {
     const { properties } = prop.signature;
-    const ret = render(prop.signature.return);
+    const ret = this.caseRender(prop.signature.return);
     return () => ret;
-  }
+  };
 
-  return (
-    <div>
-      <Collapse>{renderInspector(props.propsAst, props.snapshot)}</Collapse>
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <Collapse>
+          {this.renderInspector(this.props.propsAst, this.props.snapshot)}
+        </Collapse>
+      </div>
+    );
+  }
 }
 
-export default TypeSelector;
+export default connect(null, { updateSnapshot })(TypeSelector);
