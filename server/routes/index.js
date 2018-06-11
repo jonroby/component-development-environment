48e74f3a7b4c9828271a2c84fabd83a0f16ddd4e
jonroby <joncaroby@gmail.com>
2018-06-10 23:32:47 -0700

    reorganize routes

---
+++ server/routes/index.js
@@ -0,0 +1,11 @@
+const getComponentData = require('./getComponentData');
+const postSnapshot = require('./postSnapshot');
+const putSnapshot = require('./putSnapshot');
+const delSnapshot = require('./delSnapshot');
+
+module.exports = {
+  getComponentData,
+  postSnapshot,
+  putSnapshot,
+  delSnapshot
+};
